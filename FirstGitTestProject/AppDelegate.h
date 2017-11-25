5e6738f1b1d666b115ef2e9533ee4578b3034da6
cengxin <120950567@qq.com>
2017-11-25 15:51:20 +0800

    本地提交

---
+++ FirstGitTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstGitTestProject
+//
+//  Created by zhengxinyuan on 2017/11/25.
+//  Copyright © 2017年 www.jiushinamezhai.com曾新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstGitTestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstGitTestProject
+//
+//  Created by zhengxinyuan on 2017/11/25.
+//  Copyright © 2017年 www.jiushinamezhai.com曾新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
